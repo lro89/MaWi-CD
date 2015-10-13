edeaf989e49014e242a152122b3a3a75887c3932
kschnei <ks204432@fh-muenster.de>
2015-10-13 15:05:35 +0100

    Angepasstes Build-Script mit Docker-Start

--- movie-database/build.sh
+++ movie-database/build.sh
@@ -7,16 +7,15 @@ sudo rm -rf node_modules
 sudo npm install
 bower install --allow-root
 sudo grunt
-dir
 mkdir opt
 cd opt
 mkdir moviedatabase
 cd moviedatabase
+mkdir shop-app
+cd ..
 sudo chmod 777 moviedatabase
 cd ..
-cd ..
-dir
 rm -rf /opt/moviedatabase/shop-app
-sudo cp  -R dist/ opt/moviedatabase/shop-app/
+sudo cp  -R dist/. opt/moviedatabase/shop-app/
 cd ..
 sudo docker-compose up
