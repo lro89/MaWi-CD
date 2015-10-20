30a07d6a516e1fb30a236a59547f9f0e775db0d9
mro- <marlen.rotert@gmail.com>
2015-10-20 21:37:11 +0200

    Update DockerPush.sh

--- Jenkins/DockerPush.sh
+++ Jenkins/DockerPush.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
+# DockerHub Login
 docker login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
- 
+
+#Tag images
 docker tag -f moviedatabase_shopgui cdzwei/mvdb_shopgui:latest 
 docker tag -f moviedatabase_shop cdzwei/mvdb_shop:latest
 docker tag -f moviedatabase_movies cdzwei/mvdb_movies:latest 
 docker tag -f moviedatabase_actors cdzwei/mvdb_actors:latest 
 docker tag -f moviedatabase_navigation cdzwei/mvdb_navigation:latest 
 docker tag -f moviedatabase_monitoring cdzwei/mvdb_monitoring:latest 
- 
+
+# Push to DockerHub
 docker push docker.io/cdzwei/mvdb_shopgui 
 docker push docker.io/cdzwei/mvdb_shop 
 docker push docker.io/cdzwei/mvdb_movies 
