518169ff82e60410f6af0d690a90ce03320ab795
mro- <marlen.rotert@gmail.com>
2015-10-20 21:34:16 +0200

    Update DockerPush.sh

--- Jenkins/DockerPush.sh
+++ Jenkins/DockerPush.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 
-docker login
-#TODO CREDENTIALS
-
-docker tag moviedatabase_shopgui cdzwei/mvdb_shopgui:latest
-docker tag moviedatabase_shop cdzwei/mvdb_shop:latest
-docker tag moviedatabase_movies cdzwei/mvdb_movies:latest
-docker tag moviedatabase_actors cdzwei/mvdb_actors:latest
-docker tag moviedatabase_navigation cdzwei/mvdb_navigation:latest
-docker tag moviedatabase_monitoring cdzwei/mvdb_monitoring:latest
-
-docker push docker.io/cdzwei/mvdb_shopgui
-docker push docker.io/cdzwei/mvdb_shop
-docker push docker.io/cdzwei/mvdb_movies
-docker push docker.io/cdzwei/mvdb_actors
-docker push docker.io/cdzwei/mvdb_navigation
+docker login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
+ 
+docker tag -f moviedatabase_shopgui cdzwei/mvdb_shopgui:latest 
+docker tag -f moviedatabase_shop cdzwei/mvdb_shop:latest
+docker tag -f moviedatabase_movies cdzwei/mvdb_movies:latest 
+docker tag -f moviedatabase_actors cdzwei/mvdb_actors:latest 
+docker tag -f moviedatabase_navigation cdzwei/mvdb_navigation:latest 
+docker tag -f moviedatabase_monitoring cdzwei/mvdb_monitoring:latest 
+ 
+docker push docker.io/cdzwei/mvdb_shopgui 
+docker push docker.io/cdzwei/mvdb_shop 
+docker push docker.io/cdzwei/mvdb_movies 
+docker push docker.io/cdzwei/mvdb_actors 
+docker push docker.io/cdzwei/mvdb_navigation 
 docker push docker.io/cdzwei/mvdb_monitoring
-
-
