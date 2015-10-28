9cef8478cd3a6dbfe21cd084946558529c407965
cd-2 <k.schneid@pc-luchs.de>
2015-10-28 14:59:57 +0100

    Update build-jenkins.sh

--- movie-database/build-jenkins.sh
+++ movie-database/build-jenkins.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 echo "Build all services..."
 #Maven rausgenommen
+echo "Verzeichnis wechseln..."
 cd movie-database-shop-app
+echo "NPM-Install start..."
 sudo npm install
+echo "Bower-Install start..."
 bower install --allow-root
+echo "GRUNT start..."
 sudo grunt
 mkdir opt
 cd opt
@@ -16,4 +20,5 @@ cd ..
 rm -rf /opt/moviedatabase/shop-app
 sudo cp  -R dist/. opt/moviedatabase/shop-app/
 cd ..
+echo "Docker-Compose build..."
 sudo docker-compose build
