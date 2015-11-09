0a47283b69cae8ac5cbf298658bee8c07d28d109
mro- <marlen.rotert@gmail.com>
2015-11-09 10:48:21 +0100

    Update dockerhub-push.sh

--- Jenkins/dockerhub-push.sh
+++ Jenkins/dockerhub-push.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
 # DockerHub Login
-docker login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
+/home/ec2-user/docker-latest login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
 
 #Tag images
-docker tag -f moviedatabase_shopgui cdzwei/mvdb_shopgui:latest 
-docker tag -f moviedatabase_shop cdzwei/mvdb_shop:latest
-docker tag -f moviedatabase_movies cdzwei/mvdb_movies:latest 
-docker tag -f moviedatabase_actors cdzwei/mvdb_actors:latest 
-docker tag -f moviedatabase_navigation cdzwei/mvdb_navigation:latest 
-docker tag -f moviedatabase_monitoring cdzwei/mvdb_monitoring:latest 
+/home/ec2-user/docker-latest tag -f moviedatabase_shopgui cdzwei/mvdb_shopgui:latest 
+/home/ec2-user/docker-latest tag -f moviedatabase_shop cdzwei/mvdb_shop:latest
+/home/ec2-user/docker-latest tag -f moviedatabase_movies cdzwei/mvdb_movies:latest 
+/home/ec2-user/docker-latest tag -f moviedatabase_actors cdzwei/mvdb_actors:latest 
+/home/ec2-user/docker-latest tag -f moviedatabase_navigation cdzwei/mvdb_navigation:latest 
+/home/ec2-user/docker-latest tag -f moviedatabase_monitoring cdzwei/mvdb_monitoring:latest 
 
 # Push to DockerHub
-docker push docker.io/cdzwei/mvdb_shopgui 
-docker push docker.io/cdzwei/mvdb_shop 
-docker push docker.io/cdzwei/mvdb_movies 
-docker push docker.io/cdzwei/mvdb_actors 
-docker push docker.io/cdzwei/mvdb_navigation 
-docker push docker.io/cdzwei/mvdb_monitoring
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_shopgui 
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_shop 
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_movies 
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_actors 
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_navigation 
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_monitoring
