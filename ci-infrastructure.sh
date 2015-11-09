9244ab09be6a715cefc10581f3a93cc37d262bfe
kschnei <ks204432@fh-muenster.de>
2015-11-09 09:58:54 +0100

    Create ci-infrastructure.sh

---
+++ ci-infrastructure.sh
@@ -0,0 +1,18 @@
+# Jenkins
+sudo yum update
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+sudo yum install jenkins
+sudo service jenkins start
+sudo chkconfig jenkins on
+
+# Weitere Abhängigkeiten
+sudo yum install nodejs npm --enablerepo=epel 
+sudo npm install -g npm 
+sudo npm install -g grunt-cli  
+sudo npm install -g bower 
+sudo npm install grunt --save-dev 
+sudo yum install -y git 
+sudo wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo 
+sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo 
+sudo yum install -y apache-maven 
