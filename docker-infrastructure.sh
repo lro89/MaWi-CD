394c678fd6c214d622c86e9cde27dc443ffe34cd
kschnei <ks204432@fh-muenster.de>
2015-11-10 14:15:17 +0100

    Update docker-infrastructure.sh

--- docker-infrastructure.sh
+++ docker-infrastructure.sh
@@ -3,6 +3,8 @@
 sudo yum update -y 
 wget https://get.docker.com/builds/Linux/x86_64/docker-latest
 chmod +x docker-latest
+sudo  killall ./docker-latest
+sudo  killall docker
 sudo service cgconfig restart
 sudo nohup ./docker-latest daemon
 sudo pip install -U docker-compose
