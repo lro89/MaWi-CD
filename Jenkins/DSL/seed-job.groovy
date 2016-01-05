64dcdb57f310541cc64d9eff7c8edd819a49000c
Ruszo <ruslanwidemann@googlemail.com>
2016-01-05 10:41:26 +0100

    Update seed-job.groovy

--- Jenkins/DSL/seed-job.groovy
+++ Jenkins/DSL/seed-job.groovy
@@ -3,13 +3,49 @@ def repos = new groovy.json.JsonSlurper().parse(gitAPI.newReader())
 def artifactRepoURL = "http://52.29.31.102:8081/content/repositories/snapshots/"
 def artifactRepoID = "nexus"
 
+//Namen der jeweiligen Repo- bzw. Projekte
+def moviesRepoName = "movie-database-movies"
+def actorsRepoName = "movie-database-actors"
+
+
+/*
+  PROJEKTSPEZIFISCHE SHELL-BEFEHLE
+*/
+
+//Befehl zum Auslesen der Commit-IDs (speziell für den movies-Job !)
+def moviesCmd_PostBuild_ReadCommitIDs = '''cd /var/lib/jenkins/jobs/movie-database-actors/workspace/
+# Nur ein ">" damit die Datei zunächst überschrieben wird
+echo "GIT_ACTORS=$(git log | head -1 | sed s/'commit '//)" > $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-shop-app/workspace/
+sudo echo "GIT_SHOP_APP=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-shop-rest/workspace/
+sudo echo "GIT_SHOP_REST=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-monitoring/workspace/
+sudo echo "GIT_MONITORING=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-navigation/workspace/
+sudo echo "GIT_NAVIGATION=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties'''
+
+//Befehl zum Auslesen der Commit-IDs (speziell für den actors-Job !)
+def actorsCmd_PostBuild_ReadCommitIDs = '''cd /var/lib/jenkins/jobs/movie-database-movies/workspace/
+# Nur ein ">" damit die Datei zunächst überschrieben wird
+echo "GIT_MOVIES=$(git log | head -1 | sed s/'commit '//)" > $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-shop-app/workspace/
+sudo echo "GIT_SHOP_APP=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-shop-rest/workspace/
+sudo echo "GIT_SHOP_REST=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-monitoring/workspace/
+sudo echo "GIT_MONITORING=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
+cd /var/lib/jenkins/jobs/movie-database-navigation/workspace/
+sudo echo "GIT_NAVIGATION=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties'''
+
+
 repos.each
 {
   def name = it.name
   def gitCloneUrl = it.clone_url	//Die Git-URL, die zum Klonen verwendet wird (d.h. mit *.git am Ende)
   def gitProjectUrl = it.html_url	//Die normale Web-URL des Projekts/Repos
   
-  if(name == "movie-database-movies" | name == "movie-database-actors") 
+  if(name == moviesRepoName | name == actorsRepoName) 
   {
     println "Erstelle Job: ${name}"
     println "Git-Clone-URL: ${gitCloneUrl}"
@@ -53,30 +89,10 @@ repos.each
         switch(name)
         {
           case "movie-database-movies":
-            shellCmd_GitCommits = '''cd /var/lib/jenkins/jobs/movie-database-actors/workspace/
-# Nur ein ">" damit die Datei zunächst überschrieben wird
-echo "GIT_ACTORS=$(git log | head -1 | sed s/'commit '//)" > $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-shop-app/workspace/
-sudo echo "GIT_SHOP_APP=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-shop-rest/workspace/
-sudo echo "GIT_SHOP_REST=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-monitoring/workspace/
-sudo echo "GIT_MONITORING=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-navigation/workspace/
-sudo echo "GIT_NAVIGATION=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties'''
+            shellCmd_GitCommits = moviesCmd_PostBuild_ReadCommitIDs
             break
           case "movie-database-actors":
-            shellCmd_GitCommits = '''cd /var/lib/jenkins/jobs/movie-database-movies/workspace/
-# Nur ein ">" damit die Datei zunächst überschrieben wird
-echo "GIT_MOVIES=$(git log | head -1 | sed s/'commit '//)" > $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-shop-app/workspace/
-sudo echo "GIT_SHOP_APP=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-shop-rest/workspace/
-sudo echo "GIT_SHOP_REST=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-monitoring/workspace/
-sudo echo "GIT_MONITORING=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties
-cd /var/lib/jenkins/jobs/movie-database-navigation/workspace/
-sudo echo "GIT_NAVIGATION=$(git log | head -1 | sed s/'commit '//)" >> $WORKSPACE/env.properties'''
+            shellCmd_GitCommits = actorsCmd_PostBuild_ReadCommitIDs
             break
     
         }
@@ -126,8 +142,8 @@ sudo sh dockerhub-push.sh'''
             trigger("tstage") {
                 condition('SUCCESS')
                 parameters { 
-                  predefinedProp("ACTORS_GIT_ID", "\$GIT_ACTORS")
-                  predefinedProp("MOVIES_GIT_ID", "\$GIT_COMMIT")
+                  predefinedProp("ACTORS_GIT_ID", name == actorsRepoName ? "\$GIT_COMMIT":"\$GIT_ACTORS")
+                  predefinedProp("MOVIES_GIT_ID", name == moviesRepoName ? "\$GIT_COMMIT":"\$GIT_MOVIES")
                   predefinedProp("SHOP_REST_GIT_ID", "\$GIT_SHOP_REST")
                   predefinedProp("SHOP_APP_GIT_ID", "\$GIT_SHOP_APP")
                   predefinedProp("NAVIGATION_GIT_ID", "\$GIT_NAVIGATION")
